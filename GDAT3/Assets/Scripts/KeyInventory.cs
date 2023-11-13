7928975cbbd0328693d00d413b318c30f36e0680
Junopur <junopurs@gmail.com>
2023-11-13 14:10:51 +0800

    Work at TAFE Monday

---
+++ GDAT3/Assets/Scripts/KeyInventory.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KeySystem
+{
+    public class Key : MonoBehaviour
+    {
+        public bool has RedKey = false;
+    }
+ 
