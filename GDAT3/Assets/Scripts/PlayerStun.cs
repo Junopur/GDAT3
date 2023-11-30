74f76f03b3508e4c393fce4edf66249cedbb1f59
CanonOverseer <agent6841@gmail.com>
2023-11-30 18:14:12 +0800

    Yes

--- GDAT3/Assets/Scripts/PlayerStun.cs
+++ GDAT3/Assets/Scripts/PlayerStun.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 public class PlayerStun : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask enemyLayerMask; // Layers to fire the ray on
     private Camera _camera; // Initialized in awake because Camera.main is expensive
     private void Awake()
     {
@@ -23,9 +26,11 @@ public class PlayerStun : MonoBehaviour
     {
         Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 10, 10))
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, 10, enemyLayerMask, QueryTriggerInteraction.Collide))
         {
             Debug.DrawLine(ray.origin, hit.point, Color.green, 10, false);
+            Debug.Log($"Hit: {hit.transform.name}");
             
             if (hit.transform.CompareTag("Enemy"))
             {
